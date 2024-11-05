cedac2e423539725c4f2dd41302e4ccd26806928
Tugus Artaa <2215323039@pnb.ac.id>
2024-11-05 13:48:55 +0800

    Add controller

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\EmailLogController;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/login', function () {
-    return Inertia::render('Login'); 
+    return Inertia::render('Login');
 })->name('login')->middleware('guest');
 
 Route::post('/logout', function (Request $request) {
