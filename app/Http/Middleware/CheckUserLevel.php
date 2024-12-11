f5574a52057b78d5aa3b5388580d184a4f225bd2
diomahardika <diomahardikaaaa@gmail.com>
2024-12-11 22:00:19 +0800

    Add CheckUserLevel middleware to enforce user access levels and update routes accordingly

---
+++ app/Http/Middleware/CheckUserLevel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserLevel
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $level)
+    {
+        if (auth()->check() && auth()->user()->level !== $level) {
+            return redirect('/'); // Redirect ke halaman yang sesuai
+        }
+
+        return $next($request);
+    }
+}
